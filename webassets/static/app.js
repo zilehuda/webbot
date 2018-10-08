493660edfa7931241f191f4bca060cc488fac728
zilehuda <zilehuda.tariq@nexdegree.com>
2018-10-08 14:02:40 +0500

    comment out js and jquery part

--- webassets/static/app.js
+++ webassets/static/app.js
@@ -1,32 +1,32 @@
-$(document).ready(function() {
-	//feed to parse
-	var feed = "/rss";
+// $(document).ready(function() {
+// 	//feed to parse
+// 	var feed = "/rss";
 
-	$.ajax(feed, {
-		accepts:{
-			xml:"application/rss+xml"
-		},
-		dataType:"xml",
-		success:function(data) {
-			//Credit: http://stackoverflow.com/questions/10943544/how-to-parse-an-rss-feed-using-javascript
-			$(data).find("item").each(function () { // or "item" or whatever suits your feed
-				var el = buildElement($(this))
-				$("#app").append(el);
-			});
+// 	$.ajax(feed, {
+// 		accepts:{
+// 			xml:"application/rss+xml"
+// 		},
+// 		dataType:"xml",
+// 		success:function(data) {
+// 			//Credit: http://stackoverflow.com/questions/10943544/how-to-parse-an-rss-feed-using-javascript
+// 			$(data).find("item").each(function () { // or "item" or whatever suits your feed
+// 				var el = buildElement($(this))
+// 				$("#app").append(el);
+// 			});
 
 
-		}
-	});
-});
+// 		}
+// 	});
+// });
 
-var buildElement = (el) => {
-    var element = "<div id='"+el.find("guid").text()+"' class='message'>";
-    element += "<h2>" + el.find("title").text() + "</h2>";
-    if (el.find("link").text() != "") {
-        element += "<a href='" + el.find("link").text() + "'>" + el.find("description").text() + "</a>";
-    } else {
-        element += el.find("description").text();
-    }
-    element += "</div>";
-    return element;
-}
+// var buildElement = (el) => {
+//     var element = "<div id='"+el.find("guid").text()+"' class='message'>";
+//     element += "<h2>" + el.find("title").text() + "</h2>";
+//     if (el.find("link").text() != "") {
+//         element += "<a href='" + el.find("link").text() + "'>" + el.find("description").text() + "</a>";
+//     } else {
+//         element += el.find("description").text();
+//     }
+//     element += "</div>";
+//     return element;
+// }
